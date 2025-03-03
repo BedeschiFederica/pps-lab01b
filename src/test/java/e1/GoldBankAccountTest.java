9c81ba314904a7cd6e5860b637d9a47fa2fec7df
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-03-03 11:59:17 +0100

    e1 - part2 - refactor GoldBankAccount tests

--- src/test/java/e1/GoldBankAccountTest.java
+++ src/test/java/e1/GoldBankAccountTest.java
@@ -7,6 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GoldBankAccountTest extends CoreBankAccountTest {
 
+    private static final int DEPOSIT_AMOUNT = 1000;
+    private static final int LESS_THAN_BALANCE_WITHDRAW_AMOUNT = 200;
+    private static final int MORE_THAN_BALANCE_WITHDRAW_AMOUNT = 1200;
+    private static final int ILLEGAL_WITHDRAW_AMOUNT = 1700;
+
     private final BankAccountFactory factory = new BankAccountFactory();
 
     @Override
@@ -17,21 +22,20 @@ public class GoldBankAccountTest extends CoreBankAccountTest {
 
     @Test
     public void testCanWithdraw() {
-        this.account.deposit(1000);
-        this.account.withdraw(200);
-        assertEquals(800, this.account.getBalance());
+        this.account.deposit(DEPOSIT_AMOUNT);
+        this.account.withdraw(LESS_THAN_BALANCE_WITHDRAW_AMOUNT);
+        assertEquals(DEPOSIT_AMOUNT - LESS_THAN_BALANCE_WITHDRAW_AMOUNT, this.account.getBalance());
     }
 
     @Test
-    public void testCanWithdrawMoreThanAvailable(){
-        this.account.deposit(1000);
-        assertDoesNotThrow(() -> this.account.withdraw(1200));
+    public void testCanWithdrawMoreThanAvailable() {
+        this.account.deposit(DEPOSIT_AMOUNT);
+        assertDoesNotThrow(() -> this.account.withdraw(MORE_THAN_BALANCE_WITHDRAW_AMOUNT));
     }
 
     @Test
-    public void testCannotWithdrawMoreThanAllowed(){
-        this.account.deposit(1000);
-        assertThrows(IllegalStateException.class, () -> this.account.withdraw(1700));
+    public void testCannotWithdrawMoreThanAllowed() {
+        this.account.deposit(DEPOSIT_AMOUNT);
+        assertThrows(IllegalStateException.class, () -> this.account.withdraw(ILLEGAL_WITHDRAW_AMOUNT));
     }
-
 }
