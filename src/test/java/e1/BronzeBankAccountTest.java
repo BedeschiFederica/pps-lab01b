9b5f7f37ac353732a077ac73d30ed9e79975f54c
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-03-04 14:15:55 +0100

    e1 - part2 - refactor

--- src/test/java/e1/BronzeBankAccountTest.java
+++ src/test/java/e1/BronzeBankAccountTest.java
@@ -9,25 +9,25 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BronzeBankAccountTest extends CoreBankAccountTest {
 
     private static final int DEPOSIT_AMOUNT = 1000;
-    private static final int SMALL_WITHDRAW_AMOUNT = 50;
 
     private final BankAccountFactory factory = new BankAccountFactory();
 
     @Override
     @BeforeEach
-    void init(){
+    void init() {
         this.account = this.factory.createBronzeBankAccount();
     }
 
     @Test
     public void testCanWithdrawSmallAmount() {
+        final int smallWithdrawAmount = 50;
         this.account.deposit(DEPOSIT_AMOUNT);
-        this.account.withdraw(SMALL_WITHDRAW_AMOUNT);
-        assertEquals(DEPOSIT_AMOUNT - SMALL_WITHDRAW_AMOUNT, this.account.getBalance());
+        this.account.withdraw(smallWithdrawAmount);
+        assertEquals(DEPOSIT_AMOUNT - smallWithdrawAmount, this.account.getBalance());
     }
 
     @Test
-    public void testCannotWithdrawMoreThanAvailable(){
+    public void testCannotWithdrawMoreThanAvailable() {
         this.account.deposit(DEPOSIT_AMOUNT);
         assertThrows(IllegalStateException.class, () -> this.account.withdraw(1200));
     }
